a6d34c51283d14f2f6d6bfb1a6f08f31a4c09f1d
ganeshekhe <ganeshekhe2002@gmail.com>
2025-08-18 12:01:08 +0530

    migrated TailwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ServiceProvider } from "./context/ServiceContext";
 
-import "./index.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
